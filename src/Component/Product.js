621c4bd917398282e912c404f8805403bbe8911c
georgeuwabor <63528996+georgeuwabor@users.noreply.github.com>
2023-02-15 15:16:39 -0500

    react-event

--- src/Component/Product.js
+++ src/Component/Product.js
@@ -1,48 +1,96 @@
+import axios from "axios";
+import { useState } from "react";
+
 const Product = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState("");
+
+  let data = { title, description, price, image };
+  console.log(data);
+
+  const [error, setError] = useState("");
+
+  const handleForm = async (e) => {
+    e.preventDefault();
+
+    try {
+      let response = await axios.post(
+        "https://dummyjson.com/products/add",
+        data
+      );
+
+      console.log(response);
+    } catch (error) {
+      setError(error);
+      console.log(error);
+    }
+  };
+
   return (
     <>
-      <form id="product_container">
-        <div id="container-label">
-          <h1 className="products-details">Product details</h1>
+      <form onSubmit={handleForm}>
+        <p>{error}</p>
+        <div id="product_container">
+          <div id="container-label">
+            <h1 className="products-details">Product details</h1>
+          </div>
+          <div id="container-info">
+            <p className="text-paragraph">
+              Please fill your information so we can add your product
+            </p>
+          </div>
+          <hr className="hr"></hr>
+          <div id="label">
+            <h3 className="title">title</h3>
+          </div>
+          <div id="label2">
+            <input
+              onChange={(e) => setTitle(e.target.value)}
+              className="input"
+              type="text"
+              placeholder="enter title"
+            />
+          </div>
+          <div id="label">
+            <h3 className="title">description</h3>
+          </div>
+          <div id="label2">
+            <input
+              onChange={(e) => setDescription(e.target.value)}
+              className="input"
+              type="text"
+              placeholder="enter description"
+            />
+          </div>
+          <div id="label">
+            <h3 className="title">price</h3>
+          </div>
+          <div id="label2">
+            <input
+              onChange={(e) => setPrice(e.target.value)}
+              className="input"
+              type="text"
+              placeholder="enter price"
+            />
+          </div>
+          <div id="label">
+            <h3 className="title">image</h3>
+          </div>
+          <div id="label2" style={{ marginBottom: "60px" }}>
+            <input
+              onChange={(e) => setImage(e.target.value)}
+              className="input"
+              type="text"
+              placeholder="your image link"
+            />
+          </div>
         </div>
-        <div id="container-info">
-          <p className="text-paragraph">
-            Please fill your information so we can add your product
-          </p>
-        </div>
-        <hr className="hr"></hr>
-        <div id="label">
-          <h3 className="title">title</h3>
-        </div>
-        <div id="label2">
-          <input className="input" type="text" placeholder="enter title" />
-        </div>
-        <div id="label">
-          <h3 className="title">description</h3>
-        </div>
-        <div id="label2">
-          <input
-            className="input"
-            type="text"
-            placeholder="enter description"
-          />
-        </div>
-        <div id="label">
-          <h3 className="title">price</h3>
-        </div>
-        <div id="label2">
-          <input className="input" type="text" placeholder="enter price" />
-        </div>
-        <div id="label">
-          <h3 className="title">image</h3>
-        </div>
-        <div id="label2" style={{ marginBottom: "90px" }}>
-          <input className="input" type="text" placeholder="your image link" />
+        <div>
+          <button id="btn-btn">SEND</button>
         </div>
       </form>
-      <div id="btn-btn">
-        <button>SEND</button>
-      </div>
     </>
   );
 };
